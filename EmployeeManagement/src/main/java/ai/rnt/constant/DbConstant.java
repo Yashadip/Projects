f4a6ce035f52cd9fdb0ad54db0f6ece1265914a2
Yashadip <57977440+Yashadip@users.noreply.github.com>
2022-06-28 11:50:00 +0530

    Delete EmployeeManagement directory

--- EmployeeManagement/src/main/java/ai/rnt/constant/DbConstant.java
+++
@@ -1,8 +0,0 @@
-package ai.rnt.constant;
-
-public class DbConstant {
-     public static String CLASS_NAME = "com.mysql.jdbc.Driver" ;
-     public static String DB_USER= "root ";
-     public static String DB_PASSWORD= "root" ;
-     public static String DB_URL = "jdbc:mysql://localhost:3306/employee_management" ;
-}
